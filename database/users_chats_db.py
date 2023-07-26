1da8540f76b83d2050b8e7ee478622fbd35d98a2
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-26 18:01:56 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -7,8 +7,8 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.user
-        self.grp = self.db.groups
+        self.col = self.db.userinfo
+        self.grp = self.db.groupinfo
 
     def new_user(self, id, name):
         return dict(
